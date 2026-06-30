0d20b8506b16db5d31ff626061e568ed8b00a37f
naitiiik31 <patelnaitikkumar05@gmail.com>
2026-06-30 11:06:50 +0530

    Time: 0 ms (100%), Space: 41.9 MB (83.05%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,13 +1,20 @@
 class Solution {
-    public int numTrees(int n) {
-        //f(n)=f(i-1)*f(n-i)
-
+    public int c(int n,int[] dp){
         int ans=0;
         if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
 
         for(int i=1;i<=n;i++){
-            ans+=numTrees(i-1)*numTrees(n-i);
+            ans+=c(i-1,dp)*c(n-i,dp);
         }
-        return ans;
+
+        return dp[n]=ans;
+    }
+    public int numTrees(int n) {
+        //f(n)=f(i-1)*f(n-i)
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return c(n,dp);
+        
     }
 }
