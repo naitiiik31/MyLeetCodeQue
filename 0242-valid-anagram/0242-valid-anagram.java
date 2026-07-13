0e8bfd007c649084811c1564cbba0855610bcedf
naitiiik31 <patelnaitikkumar05@gmail.com>
2026-07-13 19:50:28 +0530

    Time: 2 ms (99.61%), Space: 44.8 MB (55.18%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] sfreq=new int[26];
+        int[] tfreq=new int[26];
+        for(char ch:s.toCharArray()){
+            sfreq[ch-'a']++;
+        }
+        for(char ch:t.toCharArray()){
+            tfreq[ch-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(sfreq[i]!=tfreq[i]) return false;
+        }
+        return true;
+    }
+}
