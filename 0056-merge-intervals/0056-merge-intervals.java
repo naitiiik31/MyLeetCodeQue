84ded1c35a330616e314b2b4c7de23191a448cb4
Naitikkumar J Patel <patelnaitikkumar05@gmail.com>
2026-09-09 09:25:20 +0530

    Time: 9 ms (34.66%), Space: 48.6 MB (97.62%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] merge(int[][] mat) {
+        Arrays.sort(mat,(a,b)->Integer.compare(a[0],b[0]));
+        int n=mat.length;
+        int[][] ans=new int[n][2];
+        int[][] res=new int[n][2];
+        res[0]=mat[0];
+        int e=0;
+        
+        for(int i=1;i<n;i++){
+            if(mat[i][0]<=res[e][1]){
+                res[e][1]=Math.max(res[e][1],mat[i][1]);
+            }else{
+                e++;
+                res[e]=mat[i];
+            }
+        }
+        return Arrays.copyOfRange(res,0,e+1);
+
+    }
+}
